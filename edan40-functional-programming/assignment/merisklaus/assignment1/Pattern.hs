b4aa8e0f8d61ccef08ed5d372a0ac1e35349752e
Meris <meris.bahti@gmail.com>
2014-11-13 11:39:16 +0100

    add stuff in proper dir

--- edan40-functional-programming/assignment/merisklaus/assignment1/Pattern.hs
+++ edan40-functional-programming/assignment/merisklaus/assignment1/Pattern.hs
@@ -8,26 +8,50 @@ import Utilities
 
 -- Replaces a wildcard in a list with the list given as the third argument
 substitute :: Eq a => a -> [a] -> [a] -> [a]
-substitute x (y:ys) z
-	| x == y = z ++ substitute x ys z
-	| otherwise = y : substitute z ys z
+substitute wc (x:xs) s 
+  | wc == x   = s ++ xs
+  | otherwise = x : substitute wc xs s
 substitute _ _ _ = []
-{- WRITTEN -}
 
 
 -- Tries to match two lists. If they match, the result consists of the sublist
 -- bound to the wildcard in the pattern list.
 match :: Eq a => a -> [a] -> [a] -> Maybe [a]
-match x 
-match _ _ _ = Nothing
+match _ [] [] = Just [] 
+match _ [] xs = Nothing
+match _ ps [] = Nothing
+match wc (p:ps) (y:ys)
+  | p == wc = orElse (singleWildcardMatch (p:ps) (y:ys)) (longerWildcardMatch (p:ps) (y:ys))
+  | otherwise = match wc ps ys
+   where
+    singleWildcardMatch :: Eq a => [a] -> [a] -> Maybe [a]
+    singleWildcardMatch (p:ps) (x:xs)
+      | ps == xs  = Just [x]
+      | otherwise = Nothing
+    longerWildcardMatch pls (xl:xls) = mmap ([xl]++) (match wc pls xls)
+    longerWildcardMatch _ _ = Nothing
+
 {- TO BE WRITTEN -}
 
+singleWildcardMatch :: Eq a => [a] -> [a] -> Maybe [a]
+singleWildcardMatch (p:ps) (x:xs)
+  | ps == xs  = Just [x]
+  | otherwise = Nothing
+-- match wc (x:xs) (y:ls)
+--  | wc == x   = Just $ extract xs y 
+ -- | otherwise = match wc xs ls
+  -- where
+   -- extract :: Eq a => [a] -> a -> [a]
+--    extract (x:xs) stop 
+ --   | x == stop = [] 
+  --  | otherwise = x : extract xs stop
+   -- extract _ _ = [] 
 
 -- Helper function to match
-singleWildcardMatch, longerWildcardMatch :: Eq a => [a] -> [a] -> Maybe [a]
-singleWildcardMatch (wc:ps) (x:xs) = Nothing
+--singleWildcardMatch, longerWildcardMatch :: Eq a => [a] -> [a] -> Maybe [a]
+--singleWildcardMatch (wc:ps) (x:xs) = Nothing
 {- TO BE WRITTEN -}
-longerWildcardMatch (wc:ps) (x:xs) = Nothing
+--longerWildcardMatch (wc:ps) (x:xs) = Nothing
 {- TO BE WRITTEN -}
 
 
