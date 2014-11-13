556f8346f1bb54304d1df859ba577fa16134be0a
Julian Kroné <julian.krone@hotmail.com>
2014-11-13 11:31:36 +0100

    gitignore and partial match

--- edan40-functional-programming/assignment/timjules/assignment1/Pattern.hs
+++ edan40-functional-programming/assignment/timjules/assignment1/Pattern.hs
@@ -1,5 +1,6 @@
 module Pattern where
 import Utilities
+import Data.List
 
 
 -------------------------------------------------------
@@ -7,10 +8,10 @@ import Utilities
 --------------------------------------------------------
 
 -- Replaces a wildcard in a list with the list given as the third argument
-substitute :: Eq a => a -> [a] -> [a] -> [a]
+substitute :: Eq(a) => a -> [a] -> [a] -> [a]
 substitute x (y:ys) z
 	| x == y = z ++ substitute x ys z
-	| otherwise = y : substitute z ys z
+	| otherwise = y : substitute x ys z
 substitute _ _ _ = []
 {- WRITTEN -}
 
@@ -18,17 +19,27 @@ substitute _ _ _ = []
 -- Tries to match two lists. If they match, the result consists of the sublist
 -- bound to the wildcard in the pattern list.
 match :: Eq a => a -> [a] -> [a] -> Maybe [a]
-match x 
-match _ _ _ = Nothing
+match n xs ys 
+  | xs == ys = Just []
+  | not $ n `elem` xs = Nothing
+  | otherwise = head $ matchHelper n xs ys
+  where
+    matchHelper n (x:xs) (y:ys)
+      | x == y = matchHelper n xs ys
+      | n == x = singleWildcardMatch (x:xs) (y:ys) `orElse` longerWildcardMatch (x:xs) (y:ys) ++ match n xs ys
+      | otherwise = Nothing
 {- TO BE WRITTEN -}
 
 
 -- Helper function to match
 singleWildcardMatch, longerWildcardMatch :: Eq a => [a] -> [a] -> Maybe [a]
-singleWildcardMatch (wc:ps) (x:xs) = Nothing
-{- TO BE WRITTEN -}
-longerWildcardMatch (wc:ps) (x:xs) = Nothing
-{- TO BE WRITTEN -}
+singleWildcardMatch (wc:ps) (x:xs)
+  | ps == xs = Just [x]
+  | otherwise = Nothing
+longerWildcardMatch (wc:ps) (x:xs)
+  | singleWildcardMatch (wc:ps) (x:xs) == Just [x] = Nothing
+  | isSuffixOf ps xs = Just (x:(take (length xs - length ps) xs))
+  | otherwise = Nothing
 
 
 
