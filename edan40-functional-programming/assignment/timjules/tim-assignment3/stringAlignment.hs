8d0e18cf01aff6601294ab5074bb1a17cd286ae5
Dolck <tim.dolck@gmail.com>
2014-12-14 18:09:13 +0100

    something

--- edan40-functional-programming/assignment/timjules/tim-assignment3/stringAlignment.hs
+++ edan40-functional-programming/assignment/timjules/tim-assignment3/stringAlignment.hs
@@ -1,3 +1,4 @@
+--String Alignment
 --By Tim Dolck
 
 import Data.List
@@ -9,9 +10,7 @@ import Data.Ord
 --had access to an algorithm for the string alignment problem, how could we use it to solve MCS for strings?
 
 --Ans: Set score match to 1 and score mismatch and score space to 0; Then if we'd run the algorithm with theese
---     values the result would be the maximal common subsequence
-align1a = "HASKELL"
-align1b = "PASCAL"
+--     values the result would be the maximal common subsequence. Thus only matches increases the score
 
 scoreMatch = 1
 scoreMismatch = -1
@@ -63,13 +62,17 @@ optAlignments (x:xs) (y:ys) = maximaBy sumScore allPoss
 
 --2e
 outputOptAlignments :: String -> String -> IO ()
-outputOptAlignments s1 s2 = mapM_ printAlignment $ optFastAlignments s1 s2
+outputOptAlignments = (.) (mapM_ printAlignment) . optFastAlignments
   where
     printAlignment :: AlignmentType -> IO ()
-    printAlignment (a, b) = do putStrLn (a ++ "\n" ++ b ++ "\n")
+    printAlignment (a, b) = putStrLn (a ++ "\n" ++ b ++ "\n")
 
 
---3 fast solution
+--3 
+attachTails :: a -> a -> [([a],[a])] ->[([a],[a])]
+attachTails t1 t2 list = [(xs ++ [t1], ys ++ [t2]) | (xs,ys) <- list]
+
+--fast solution
 optFastAlignments :: String -> String -> [AlignmentType]
 optFastAlignments s1 s2 = snd $ tableGet (length s1) (length s2)
   where
@@ -90,35 +93,3 @@ optFastAlignments s1 s2 = snd $ tableGet (length s1) (length s2)
                               attachAndAdd scoreMismatch '-' (s2s j) $ tableGet i (j-1)]
     s1s i = s1 !! (i-1)
     s2s j = s2 !! (j-1)
-
-    
-
-attachTails :: a -> a -> [([a],[a])] ->[([a],[a])]
-attachTails t1 t2 list = [(xs ++ [t1], ys ++ [t2]) | (xs,ys) <- list]
-
-
-mcsLength :: Eq a => [a] -> [a] -> Int
-mcsLength xs ys = mcsLen (length xs) (length ys)
-  where
-    mcsLen i j = mcsTable!!i!!j
-    mcsTable = [[ mcsEntry i j | j<-[0..]] | i<-[0..] ]
-       
-    mcsEntry :: Int -> Int -> Int
-    mcsEntry _ 0 = 0
-    mcsEntry 0 _ = 0
-    mcsEntry i j
-      | x == y    = 1 + mcsLen (i-1) (j-1)
-      | otherwise = max (mcsLen i (j-1)) 
-                        (mcsLen (i-1) j)
-      where
-         x = xs!!(i-1)
-         y = ys!!(j-1)
-
-
-
-
-
-
-
-
-
