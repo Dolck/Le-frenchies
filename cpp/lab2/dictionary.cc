61ffc51873b4163c73007679e674dddaca4842f6
Dolck <tim.dolck@gmail.com>
2015-02-09 13:56:30 +0100

    almost done but seg fault

--- cpp/lab2/dictionary.cc
+++ cpp/lab2/dictionary.cc
@@ -27,12 +27,12 @@ Dictionary::Dictionary() {
 			for (int i = 0; i < c; ++i)
 			{
 				pos = line.find(" ");
-				push_back(line.substr(0, pos));
+				trigrams.push_back(line.substr(0, pos));
 				line.erase(0, pos + 1);
 			}
 			Word w(word, trigrams);
 			if(word.length() <= MAX_SIZE)
-				words[words.length()].push_back(w);
+				words[word.length()].push_back(w);
 		}
 	}
 
@@ -41,10 +41,75 @@ Dictionary::Dictionary() {
 bool Dictionary::contains(const string& word) const {
 	auto found = allwords.find(word);
 
-	return found != words.end();
+	return found != allwords.end();
 }
 
 vector<string> Dictionary::get_suggestions(const string& word) const {
 	vector<string> suggestions;
+	add_trigram_suggestions(suggestions, word);
+	rank_suggestions(suggestions, word);
 	return suggestions;
+}
+
+void Dictionary::add_trigram_suggestions(vector<string> suggestions, const string& word) const{
+	//get size of word
+	int size = word.size();
+	vector<string> wordtris;
+
+	if(size > 2){
+		//get trigrams
+		for(unsigned long i = 0; i <= word.length() - 3; ++i){
+			wordtris.push_back(word.substr(i, 3));
+		}
+		//sort trigrams
+		sort(wordtris.begin(), wordtris.end());
+		//get list of words -1/=/+1 size
+		for(int i = size-1; i<=size+1 && i < MAX_SIZE; ++i){
+			for(Word w : words[i]){
+				//exract words that have atleast 50% trigrams that exists in word
+				if(w.get_matches(wordtris) >= static_cast<float>(wordtris.size()) / 2.0){
+					suggestions.push_back(w.get_word());
+				}
+			}
+		}
+	}
+}
+
+void Dictionary::rank_suggestions(vector<string> suggestions, const string& word) const{
+	vector<pair<string, int>> pairs;
+
+	for(string s : suggestions){
+		int d[26][26];
+		d[0][0] = 0;
+		for (int x = 1; x < 26; ++x) {
+			d[x][0] = x;
+			d[0][x] = x;
+		}
+
+		for (int x = 1; x <= word.length(); ++x)
+		{
+			for (int y = 1; y <= s.length(); ++y)
+			{
+				int diag = word[x] == s[y] ? d[x-1][y-1] : 1 + d[x-1][y-1];
+				d[x][y] = min(min(diag, d[x-1][y] + 1), d[x][y-1] + 1);
+			}
+		}
+
+		pair<string, int> p (s, d[word.length()][s.length()]);
+		pairs.push_back(p);
+	}
+
+	sort(pairs.begin(), pairs.end(), compair);
+
+	suggestions.clear();
+
+	//trim top 5
+	for (int i = 0; i < 5; ++i)
+	{
+		suggestions.push_back(pairs[i].first);
+	}
+}
+
+bool Dictionary::compair(const pair<string,int>& p1, const pair<string,int>& p2){
+	return p1.second < p2.second;
 }
--- cpp/lab2/dictionary.h
+++ cpp/lab2/dictionary.h
@@ -15,8 +15,11 @@ public:
 	bool contains(const std::string& word) const;
 	std::vector<std::string> get_suggestions(const std::string& word) const;
 private:
+	void add_trigram_suggestions(vector<string> suggestions, const string& word) const; // can we have const here?
+	void rank_suggestions(vector<string> suggestions, const string& word) const; // can we have const here?
+	static bool compair(const pair<string, int>& p1, const pair<string, int>& p2);
 	unordered_set<string> allwords;
-	vector<Word> words[MAX_SIZE]; 
+	vector<Word> words[MAX_SIZE + 1]; 
 };
 
 #endif
