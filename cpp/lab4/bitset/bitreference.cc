2708c10042eeab5a2781c0021bf2e85dbaee4cab
Dolck <tim.dolck@gmail.com>
2015-02-23 15:28:05 +0100

    some fix

--- cpp/lab4/bitset/bitreference.cc
+++ cpp/lab4/bitset/bitreference.cc
@@ -6,6 +6,12 @@ BitReference& BitReference::operator=(bool b) {
 	// *** IMPLEMENT ***
 	// This corresponds to the set() function in SimpleBitset.
 	//
+	if(b){
+		p_bits |= 1L << pos;
+	}else{
+		p_bits &= ~ (1L << pos)
+	}
+
 	return *this;
 }
 
