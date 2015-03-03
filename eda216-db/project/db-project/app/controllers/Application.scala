d6539f7aad6bc728e660ee4502679aa2c2ac0566
Dolck <tim.dolck@gmail.com>
2015-03-03 21:19:44 +0100

    list pallets

--- eda216-db/project/db-project/app/controllers/Application.scala
+++ eda216-db/project/db-project/app/controllers/Application.scala
@@ -6,6 +6,8 @@ import java.util.Date
 import models._
 import play.api.data._
 import play.api.data.Forms._
+import java.text.DateFormat
+import java.text.SimpleDateFormat
 
 object Application extends Controller {
 
@@ -13,9 +15,37 @@ object Application extends Controller {
     Ok(views.html.index("Your new application is ready."))
   }
 
+  def listPallets(fromDate: String, toDate: String, status: String, cookie: String) = Action {
+  	var format: DateFormat = new SimpleDateFormat("yyyyMMdd:HHmmss")
+  	var fDate: Date = new Date(Long.MinValue)
+  	var tDate: Date = new Date(Long.MinValue)
+  	if(fromDate != "")
+		fDate = format.parse(fromDate)
+  	if(toDate != "")
+  		tDate = format.parse(toDate)
+  	//Call database and retreive list of pallets
+
+  	//temp data:
+  	val p1: Pallet = new Pallet(1, new Date(), "Cookie", PalletStatus.Free)
+  	val p2: Pallet = new Pallet(2, new Date(), "Cookie", PalletStatus.Free)
+  	val p3: Pallet = new Pallet(3, new Date(), "Cookie", PalletStatus.Free)
+  	val p4: Pallet = new Pallet(4, new Date(), "Cookie", PalletStatus.Free)
+  	val ps = Array(p1, p2, p3, p4)
+  	
+  	Ok(views.html.palletList(ps))
+
+  }
+
   def pallet(id: Int) = Action {
   	val p: Pallet = new Pallet(id, new Date(), "Cookie1", PalletStatus.Free)
     Ok(views.html.pallet(p))
+  }
+
+  def updatePallet = Action { implicit request =>
+    val (status, id) = palletForm.bindFromRequest.get
+    //Call database here and update pallet status
+    //Then show pallet view again?
+    Ok("Pallet updated")
   }
 
   val palletForm = Form(
@@ -24,12 +54,4 @@ object Application extends Controller {
       "id" -> text
     )
   )
-
-  def updatePallet = Action { implicit request =>
-    val (status, id) = palletForm.bindFromRequest.get
-    //Call database here and update pallet status
-    //Then show pallet view again
-    Ok("Pallet updated")
-  }
-
 }
