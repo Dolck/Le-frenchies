619112553cdf035a8d11792a9ae57ee39847e098
Dolck <tim.dolck@gmail.com>
2015-03-05 12:47:47 +0100

    hellofacommit

--- cpp/clientserver/serverImpl1.cc
+++ cpp/clientserver/serverImpl1.cc
@@ -2,6 +2,7 @@
 #include "server.h"
 #include "connection.h"
 #include "connectionclosedexception.h"
+#include "protocol.h"
 
 #include <memory>
 #include <iostream>
@@ -23,13 +24,63 @@ int readNumber(const shared_ptr<Connection>& conn) {
 }
 
 /*
+ * Read a char from a client
+ */
+char readChar(const shared_ptr<Connection>& conn) {
+	return conn->read();
+}
+
+/*
+ * Reads a string
+ */
+string readString(const shared_ptr<Connection>& conn, const int nbrChars) {
+	string s;
+	for (int i = 0; i < nbrChars; ++i){
+		s += readChar(conn);
+	}
+	return s;
+}
+
+/*
+ * Send an integer to the client as four bytes.
+ */
+void writeNumber(const shared_ptr<Connection>& conn, int value) {
+	conn->write((value >> 24) & 0xFF);
+	conn->write((value >> 16) & 0xFF);
+	conn->write((value >> 8)	 & 0xFF);
+	conn->write(value & 0xFF);
+}
+
+struct article {
+	string title;
+	string author;
+	string article_text;
+	unsigned int id;
+};
+
+struct newsgroup {
+	string name;
+	unsigned int id;
+	vector<article> articles;
+};
+
+bool ngExists(const vector<newsgroup>& v, const string& newTitle){
+	for(newsgroup ng : v){
+		if(ng.name == newTitle){
+			return true;
+		}
+	}
+	return false;
+}
+
+/*
  * Send a string to a client.
  */
 void writeString(const shared_ptr<Connection>& conn, const string& s) {
 	for (char c : s) {
 		conn->write(c);
 	}
-	conn->write('$');
+	//conn->write('$');
 }
 
 int main(int argc, char* argv[]){
@@ -51,11 +102,70 @@ int main(int argc, char* argv[]){
 		cerr << "Server initialization error." << endl;
 		exit(1);
 	}
+
+	vector<newsgroup> groups; // Check this?
+	int groupId = 1, articleId = 1;
 	
 	while (true) {
 		auto conn = server.waitForActivity();
 		if (conn != nullptr) {
 			try {
+				char cmd = readChar(conn);
+				switch(cmd){
+					case Protocol::COM_LIST_NG:{
+						char end = readChar(conn);
+						int num = groups.size();
+						conn->write(Protocol::ANS_LIST_NG);
+						conn->write(Protocol::PAR_NUM);
+						writeNumber(conn, num);
+						for(newsgroup ng : groups){
+							conn->write(Protocol::PAR_NUM);
+							writeNumber(conn, ng.id);
+							conn->write(Protocol::PAR_STRING);
+							writeNumber(conn, ng.name.size());
+							writeString(conn, ng.name);
+						}
+						conn->write(Protocol::ANS_END);
+						break;
+					}
+					case Protocol::COM_CREATE_NG: {
+						char c = readChar(conn);
+						if(c == Protocol::PAR_STRING){
+							int n = readNumber(conn);
+							string newTitle = readString(conn, n);
+							char end = readChar(conn);
+							if(ngExists(groups, newTitle)){
+								//Write something
+							}else{
+								newsgroup ng;
+								ng.name = newTitle;
+								ng.id = ++groupId;
+								groups.push_back(ng);
+								string output;
+								output += Protocol::ANS_CREATE_NG;
+								output += Protocol::ANS_ACK;
+								output += Protocol::ANS_END;
+								writeString(conn, output);
+							}
+						}else{
+							throw ConnectionClosedException();
+						}
+						break;
+					}
+					case Protocol::COM_DELETE_NG:
+						break;
+					case Protocol::COM_LIST_ART:
+						break;
+					case Protocol::COM_CREATE_ART:
+						break;
+					case Protocol::COM_DELETE_ART:
+						break;
+					case Protocol::COM_GET_ART:
+						break;
+					default: throw ConnectionClosedException();
+						break;
+				}
+
 				/*
 				* Communicate with a client, conn->read()
 				* and conn->write(c)
