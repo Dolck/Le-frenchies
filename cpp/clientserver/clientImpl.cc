e5c3f10441971a9fbad3f1b68aa837438a459ae1
Dolck <tim.dolck@gmail.com>
2015-03-17 16:11:10 +0100

    client stuff

--- cpp/clientserver/clientImpl.cc
+++ cpp/clientserver/clientImpl.cc
@@ -33,23 +33,14 @@ string readString(const shared_ptr<Connection>& conn) {
     return s;
 }
 
-void welcomePrompt(){
-    cout << "Welcome to our newsclient. Please choose a command: " << endl;
-    cout << "1: List newsgroups" << endl;
-    cout << "2: Create newsgroup" << endl;
-    cout << "3: Delete newsgroup" << endl;
-    cout << "4: List articles" << endl;
-    cout << "5: Create article" << endl;
-    cout << "6: Delete article" << endl;
-    cout << "Type a number: ";
-}
-
 void listNewsGroups(const shared_ptr<Connection>& conn){
     conn->write(Protocol::COM_LIST_NG);
     conn->write(Protocol::COM_END);
-    //{Protocol::ANS_LIST_NG, Protocol::PAR_NUM};
-    if(conn->read() != Protocol::ANS_LIST_NG || conn->read() != Protocol::PAR_NUM)
-        throw ConnectionClosedException();
+
+    //if(conn->read() != Protocol::ANS_LIST_NG || conn->read() != Protocol::PAR_NUM)
+    //    throw ConnectionClosedException();
+    //MessageHandler::expectInputChar(conn, Protocol::ANS_LIST_NG);
+    //int nbrNgs = MessageHandler::readNumber(conn);
 
     int nbrNgs = readInt(conn);
     cout << endl;
@@ -64,6 +55,42 @@ void listNewsGroups(const shared_ptr<Connection>& conn){
     }
 
     cout << endl;
+}
+
+void createNewsGroup(const shared_ptr<Connection>& conn){
+    cout << "Enter title: ";
+    string title;
+    cin >> title;
+    //command COM_CREATE_NG
+    //MessageHandler::writeString(conn, title);
+    //COM_END
+}
+
+void deleteNewsGroup(const shared_ptr<Connection>& conn){
+    cout << "Enter id from list above: ";
+    int id;
+    cin >> id;
+    //COM_DELETE_NG
+    //MessageHandle::writeInteger(id);
+    //COM_END
+}
+
+void listArticles(const shared_ptr<Connection>& conn){
+    cout << "Enter newsgroup id to list its articles: ";
+    int id;
+    cin >> id;
+    //
+}
+
+void welcomePrompt(){
+    cout << "Welcome to our newsclient. Please choose a command: " << endl;
+    cout << "1: List newsgroups" << endl;
+    cout << "2: Create newsgroup" << endl;
+    cout << "3: Delete newsgroup" << endl;
+    cout << "4: List articles" << endl;
+    cout << "5: Create article" << endl;
+    cout << "6: Delete article" << endl;
+    cout << "Type a number: ";
 }
 
 int main(int argc, char* argv[]) {
@@ -95,8 +122,15 @@ int main(int argc, char* argv[]) {
                     listNewsGroups(conn);
                     break;
                 case 2:
+                    createNewsGroup(conn);
+                    break;
                 case 3:
+                    listNewsGroups(conn);
+                    deleteNewsGroup(conn);
+                    break;
                 case 4:
+                    listNewsGroups(conn);
+                    listArticles(conn);
                 case 5:
                 case 6:
                 default:
