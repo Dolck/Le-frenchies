43f740e2e05d366beec5ef14dd3909a69a8d8af3
Dolck <tim.dolck@gmail.com>
2015-03-31 12:06:46 +0200

    writing file

--- cpp/clientserver/saveToFile.cc
+++ cpp/clientserver/saveToFile.cc
@@ -11,11 +11,14 @@ vector<newsgroup>& SaveToFile::readNewsgroups(int& nid, int& aid){
 void SaveToFile::writeNewgroups(const vector<newsgroup>& ngs, int nid, int aid){
 	ofstream file(fileName);
 	if(file.is_open()){		
-		myfile << ngId << endl;
-		myfile << aId << endl;
-		
-		for(newsgroup ng : ngs) {
-			myfile << 
+		myfile << nid << endl;
+		myfile << aid << endl;
+		myfile << ngs.size() << endl;
+		for(const newsgroup& ng : ngs) {
+			myfile << ng.id << "\t" << ng.name << "\t" << ng.articles.size() << "\t";
+			for(const article& a : ng.articles){
+				myfile << a.id << "\t" << a.title << "\t" << a.author << "\t" << a.article_text << "\t";
+			}
 		}
 	}
 }
